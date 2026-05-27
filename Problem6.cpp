53eb8bb8b46b4b3b0ef93a084cced6d26b93fb46
Nguyen Dang Khang <nguyendakacun@gmail.com>
2026-05-27 18:17:06 +0700

    Modify assigment for file Problem6

--- Problem6.cpp
+++ Problem6.cpp
@@ -5,14 +5,14 @@ A number spiral is an infinite grid whose upper-left square has number 1. Here a
 5  6  7  12 23
 16 15 14 13 22
 17 18 19 20 21
-Your task is to find out the number in row y and column x.
+Your task is to find out the number in row x and column y.
 
 Input:
 The first input line contains an integer t: the number of tests.
-After this, there are t lines, each containing integers y and x.
+After this, there are t lines, each containing integers x and y.
 
 Output:
-For each test, print the number in row y and column x.
+For each test, print the number in row x and column y.
 */
 #include <iostream>
 #include <algorithm>
